95e18fac6e8a4aa9f31daccd9065ba55448212bc
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-23 23:53:56 +0300

    Add project files.

---
+++ CSharp8/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Sayıyı giriniz...");
+int sayi = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (i <= sayi)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
